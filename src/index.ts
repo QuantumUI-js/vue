0bfac9ad1d618379de34c8f56fed989858b34bae
Quan Vu <huyquan.vu.1@gmail.com>
2023-05-18 17:22:06 +0700

    Simple DataTable Component

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import Button from './Button.vue';
 import Heading from './Heading.vue';
 import Modal from './Modal.vue';
 import { TreeItem } from './treeview';
+import { DataTable } from './datatable';
 
-export { Button, Heading, Modal, TreeItem };
+export { Button, Heading, Modal, TreeItem, DataTable };
