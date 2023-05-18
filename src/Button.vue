3daec0d2a778b53da7e34292efc0bef2d4075f50
Quan Vu <huyquan.vu.1@gmail.com>
2023-05-18 11:09:43 +0700

    Init Vue 3 UI Framework template

---
+++ src/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button class="button">
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+import { defineOptions } from 'vue'
+
+defineOptions({
+    name: 'QVButton',
+    props: {
+        type: {
+            type: String,
+            default: 'button',
+        },
+    },
+})
+</script>
