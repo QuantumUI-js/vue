c2104a4baa3aa594e74f4eaec6fb4025333516ab
Quan Vu <huyquan.vu.1@gmail.com>
2023-05-23 17:50:48 +0700

    Rename dir src to components

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,15 @@
-export * from '../src/index'
+import Button from './Button.vue';
+import Heading from './Heading.vue';
+import Modal from './Modal.vue';
+import { TreeItem } from './treeview';
+import { DataTable } from './datatable';
+import Spreadsheet from './spreadsheet';
+
+export {
+    Button,
+    Heading,
+    Modal,
+    TreeItem,
+    DataTable,
+    Spreadsheet,
+};
--- index.ts
+++ index.ts
@@ -1,1 +1,1 @@
-export * from './src/index'
+export * from './components/index'
