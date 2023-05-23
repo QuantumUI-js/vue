1171d6592a91832c267931ae3e190dee3a34c544
Quan Vu <huyquan.vu.1@gmail.com>
2023-05-23 17:47:23 +0700

    Add components directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts', // Entry point of your library
+      entry: 'components/index.ts', // Entry point of your library
       name: 'quantum-vue', // Name of your library
       fileName: (format) => `quantum-vue.${format}.js`, // Output file name
     },
